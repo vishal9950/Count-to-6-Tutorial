5867ab0000500096fd1f0a4d96d51bfef2c22c6e
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-21 17:33:14 +0530

    Ex2: #TC2: all CAPS, passed

--- 2_templateStrings.test.js
+++ 2_templateStrings.test.js
@@ -2,6 +2,13 @@ const greet = require('./2_templateStrings');
 
 const strings = ['Hello, ', '!\nYour name lowercased is "', '"'];
 
+// testcases
 test('Should not log if name not provided: ', () => {
   expect(greet(strings, '', '')).toBe(false);
+});
+
+const nameArg = 'XYZ';
+test('Should greet if name is all CAPS: ', () => {
+  expect(greet(strings, nameArg, nameArg.toLowerCase())).toBe(`Hello, XYZ!
+Your name lowercased is "xyz"`);
 });
