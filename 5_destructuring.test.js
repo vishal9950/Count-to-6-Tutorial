b2988ab5147644cb959e4770e66cadfbc56ddb63
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-21 22:37:04 +0530

    Ex5: #TC3: email present, passed

--- 5_destructuring.test.js
+++ 5_destructuring.test.js
@@ -9,3 +9,9 @@ const obj1 = { username: 'jdoe', email: 'jdoe@example.com' };
 test('Should work for [1, "jdoe", "jdoe@example.com", "John", "Doe"]: ', () => {
   expect(destructure(arr1)).toEqual(obj1);
 });
+
+const arr2 = [1, '', 'jdoe@example.com', 'John', 'Doe'];
+const obj2 = { username: '', email: 'jdoe@example.com' };
+test('Should work for [1, " ", "jdoe@example.com", "John", "Doe]: ', () => {
+  expect(destructure(arr2)).toEqual(obj2);
+});
