ff65fd747dae335ea56db00d045010cfd2a16ca0
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-21 23:22:33 +0530

    Ex7: #TC3: empty arrays, passed

--- 7_rest.js
+++ 7_rest.js
@@ -1,7 +1,7 @@
 module.exports = function average(...args) {
   if (args[0] === undefined) {
-    console.log('Err1: No args supplied');
-    return undefined;
+    console.log('Err1: No args supplied/ Empty array supplied');
+    return false;
   }
   let sum = 0;
   args.forEach((value) => {
--- 7_rest.test.js
+++ 7_rest.test.js
@@ -1,9 +1,13 @@
 const average = require('./7_rest');
 
 test('Should not work for no args: ', () => {
-  expect(average()).toBe(undefined);
+  expect(average()).toBe(false);
 });
 
 test('Should work for [1, 2, 3]: ', () => {
   expect(average(1, 2, 3)).toBe(2);
+});
+
+test('Should not work for empty array: ', () => {
+  expect(average()).toBe(false);
 });
