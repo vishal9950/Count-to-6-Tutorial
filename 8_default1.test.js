06da89cf9d7059c7fa771db84e79e4da467b9e33
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-21 23:47:42 +0530

    Ex8: #TC2: 1 arg, passed

--- 8_default1.test.js
+++ 8_default1.test.js
@@ -3,3 +3,7 @@ const midpoint = require('./8_default1');
 test('Should work for no args: ', () => {
   expect(midpoint()).toBe(0.5);
 });
+
+test('Should work for 1 arg: ', () => {
+  expect(midpoint(undefined, 2)).toBe(1);
+});
