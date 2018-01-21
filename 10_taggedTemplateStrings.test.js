c66a72291b492c947d7db55b27017fd41e1eaa44
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-22 01:25:10 +0530

    Ex10: #TC2: proper args, passed

--- 10_taggedTemplateStrings.test.js
+++ 10_taggedTemplateStrings.test.js
@@ -1,6 +1,12 @@
 const html = require('./10_taggedTemplateStrings');
 
-const string = ['<b>', ' says</b>: "', '"'];
+// const string = ['<b>', ' says</b>: "', '"'];
 test('Should not work for empty args: ', () => {
   expect(html()).toBe(undefined);
+});
+
+const username = 'Daniel';
+const comment = '2 < 3';
+test('Should work for <b>Daniel says</b>: "2 < 3"', () => {
+  expect(html`<b>${username} says</b>: "${comment}"`).toBe('<b>Daniel says</b>: "2 &lt; 3"');
 });
