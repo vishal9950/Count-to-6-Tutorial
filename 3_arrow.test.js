b7c48f20a2828405261d59428b21e94ae4d9e716
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-21 19:33:38 +0530

    Ex3: #TC3: string and num array, passed

--- 3_arrow.test.js
+++ 3_arrow.test.js
@@ -8,3 +8,8 @@ const arr1 = ['Hello', 'this', 'is', 'ES6'];
 test('Should work for string array: ', () => {
   expect(arrow(arr1)).toBe(`[${arr1}] becomes "HtiE"`);
 });
+
+const arr2 = ['Hello', '123'];
+test('Should work for string and num array: ', () => {
+  expect(arrow(arr2)).toBe(`[${arr2}] becomes "H1"`);
+});
