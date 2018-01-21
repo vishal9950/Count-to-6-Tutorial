6849fc0aada5975a939d77a69eca0ba0c9252e19
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-21 22:58:31 +0530

    Ex6: #TC2: proper args, passed

--- 6_spread.test.js
+++ 6_spread.test.js
@@ -3,3 +3,8 @@ const spread = require('./6_spread');
 test('Shopuld not work for no args: ', () => {
   expect(spread()).toBe(undefined);
 });
+
+const arr1 = [18, 5, 7, 24];
+test('Should work for [18,5,7,24]: ', () => {
+  expect(spread(arr1)).toBe('The minimum of [18,5,7,24] is 5');
+});
