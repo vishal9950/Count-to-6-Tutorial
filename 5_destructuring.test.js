b197dd07e9cae6dbf0d7fd1e4f31d77e23613921
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-21 22:36:29 +0530

    Ex5: #TC2: both present, passed

--- 5_destructuring.test.js
+++ 5_destructuring.test.js
@@ -3,3 +3,9 @@ const destructure = require('./5_destructuring');
 test('Should not work if no args: ', () => {
   expect(destructure()).toBe(undefined);
 });
+
+const arr1 = [1, 'jdoe', 'jdoe@example.com', 'John', 'Doe'];
+const obj1 = { username: 'jdoe', email: 'jdoe@example.com' };
+test('Should work for [1, "jdoe", "jdoe@example.com", "John", "Doe"]: ', () => {
+  expect(destructure(arr1)).toEqual(obj1);
+});
