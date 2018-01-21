7d00ecb01011c07db0350fd72e9daef9bb2dc0ac
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-21 23:17:35 +0530

    Ex7: #TC2: proper args, passed

--- 7_rest.test.js
+++ 7_rest.test.js
@@ -3,3 +3,7 @@ const average = require('./7_rest');
 test('Should not work for no args: ', () => {
   expect(average()).toBe(undefined);
 });
+
+test('Should work for [1, 2, 3]: ', () => {
+  expect(average(1, 2, 3)).toBe(2);
+});
