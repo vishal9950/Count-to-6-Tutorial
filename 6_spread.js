070e0e34cd997da2a3b18de6aae36cb27678cce6
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-21 23:00:55 +0530

    Ex6: #TC3: empty array, passed

--- 6_spread.js
+++ 6_spread.js
@@ -1,6 +1,10 @@
 function spread(args) {
   if (!args) {
+    console.log('Err1: No args supplied');
     return undefined;
+  } else if (args.length === 0) {
+    console.log('Err2: Empty array supplied');
+    return false;
   }
   const min = Math.min(...args);
   return `The minimum of [${args}] is ${min}`;
--- 6_spread.test.js
+++ 6_spread.test.js
@@ -8,3 +8,7 @@ const arr1 = [18, 5, 7, 24];
 test('Should work for [18,5,7,24]: ', () => {
   expect(spread(arr1)).toBe('The minimum of [18,5,7,24] is 5');
 });
+
+test('Should not work for empty array: ', () => {
+  expect(spread([])).toBe(false);
+});
