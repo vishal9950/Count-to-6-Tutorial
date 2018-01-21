ba78ceae99be5c9e16e369edea5f20e2653f7010
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-22 00:19:48 +0530

    Ex9: #TC3: both args, passed

--- 9_default2.test.js
+++ 9_default2.test.js
@@ -3,3 +3,11 @@ const makeImportant = require('./9_default2');
 test('Should not work if str arg is empty: ', () => {
   expect(makeImportant(undefined, 5)).toBe(false);
 });
+
+test('Should work if only str supplied: ', () => {
+  expect(makeImportant('Hello')).toBe('Hello!!!!!');
+});
+
+test('Should work if both args: ', () => {
+  expect(makeImportant('Hello', 1)).toBe('Hello!');
+});
